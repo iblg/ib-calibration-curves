7d2a23e37cf322c417c5826bf45a49434e71a02a
Ian Billinge <ian.billinge@gmail.com>
2025-06-19 16:31:28 -0400

    test: added power law test function.

--- tests/test_fits.py
+++ tests/test_fits.py
@@ -1,5 +1,9 @@
 import pytest
-from ib_calibration_curves.hplc_urea import linearfit, exponentialfit
+from ib_calibration_curves.hplc_urea import (
+    linearfit,
+    exponentialfit,
+    powerlawfit,
+)
 from pathlib import Path
 
 
@@ -12,11 +16,16 @@ def test_exponential_fit():
 
 
 def test_linear_fit():
-    p = (
-        Path() / "tests" / "linear_fit_test_data.csv"
-    )  # the function linearfit takes a Path as the only argument
-    print(p)
+    p = Path() / "tests" / "linear_fit_test_data.csv"
     y, dy, model = linearfit(p, x="x", y="y")
     result = model.params[0]
     expected = 6.9
     assert expected == pytest.approx(result, abs=1e-1)
+
+
+def test_power_law_fit():
+    p = Path() / "tests" / "power_law_fit_test_data.csv"
+    y, dy, model = powerlawfit(p, x="x", y="y")
+    result = model.params[0]
+    expected = 2.787
+    assert expected == pytest.approx(result, abs=1e-3)
