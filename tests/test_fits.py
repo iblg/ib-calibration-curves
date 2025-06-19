76eb5aa69cb190c53726ed88d7c16ab19681104f
Ian Billinge <ian.billinge@gmail.com>
2025-06-19 16:23:16 -0400

    tests: added some arguments

--- tests/test_fits.py
+++ tests/test_fits.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 def test_exponential_fit():
     p = Path() / "tests" / "linear_fit_test_data.csv"
-    y, dy, model = exponentialfit(p)
+    y, dy, model = exponentialfit(p, x="x", y="y")
     result = model.params[0]
     expected = 11.93
     assert expected == pytest.approx(result, abs=1e-2)
@@ -16,7 +16,7 @@ def test_linear_fit():
         Path() / "tests" / "linear_fit_test_data.csv"
     )  # the function linearfit takes a Path as the only argument
     print(p)
-    y, dy, model = linearfit(p)
+    y, dy, model = linearfit(p, x="x", y="y")
     result = model.params[0]
     expected = 6.9
     assert expected == pytest.approx(result, abs=1e-1)
