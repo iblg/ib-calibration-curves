5911a812c7afd1f7ca86fe7c29ff71c5bd5b8474
Ian Billinge <ian.billinge@gmail.com>
2025-06-30 12:41:01 -0400

    added bogus folder

--- tests/test_fits.py
+++ tests/test_fits.py
@@ -1,5 +1,5 @@
 import pytest
-from ib_calibration_curves.hplc_urea import (
+from ib_calibration_curves.urea import (
     linearfit,
     exponentialfit,
     powerlawfit,
@@ -7,26 +7,26 @@ from ib_calibration_curves.hplc_urea import (
 from pathlib import Path
 
 
-def test_exponential_fit():
+def test_exponential_fit(data_files):
     # Taylor page ~195
-    p = Path() / "tests" / "linear_fit_test_data.xlsx"
+    p = Path(data_files) / "exponential_fit_test_data.xlsx"
     y, dy, model = exponentialfit(p, x="x", y="y")
     result = model.params.iloc[1]
     expected = 0.089
     assert expected == pytest.approx(result, abs=1e-1)
 
 
-def test_linear_fit():
+def test_linear_fit(data_files):
     # Taylor page ~185
-    p = Path() / "tests" / "linear_fit_test_data.xlsx"
+    p = Path(data_files) / "linear_fit_test_data.xlsx"
     y, dy, model = linearfit(p, x="x", y="y")
     result = model.params.iloc[0]
     expected = 39.0
     assert expected == pytest.approx(result, abs=1e-1)
 
 
-def test_power_law_fit():
-    p = Path() / "tests" / "power_law_fit_test_data.xlsx"
+def test_power_law_fit(data_files):
+    p = Path(data_files) / "power_law_fit_test_data.xlsx"
     y, dy, model = powerlawfit(p, x="x", y="y")
     result = model.params.iloc[1]
     expected = 2.787
