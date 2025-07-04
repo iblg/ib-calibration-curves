7c9cd4445857b48c1cfe293bea7cb38892b31160
Ian Billinge <ian.billinge@gmail.com>
2025-07-04 13:04:29 -0400

    fix: fixed infinite dilution bug

--- tests/test_dilution.py
+++ tests/test_dilution.py
@@ -6,21 +6,34 @@ from ib_calibration_curves.dilution import (
     calculate_dilution,
 )
 
+test_files = [
+    "dilution_data.csv",
+    "dilution_data_no_standard_added.csv",
+    "dilution_data_no_water_added.csv",
+]
+test_files = ["dilution_test_files/" + file for file in test_files]
 
-def test_calculate_dilution_factor(data_files):
+
+@pytest.mark.parametrize("file", test_files)
+def test_calculate_dilution_factor(data_files, file):
     # Taylor page ~195
-    test_data_path = Path(data_files) / "dilution_data.csv"
+
+    # get data
+    test_data_path = Path(data_files) / file
     df = pd.read_csv(test_data_path, sep=",")
+
+    # get expected
     expected = df["dilution_factor"].iloc[0]
+
+    # calculate result
     modified_df = calculate_dilution_factor(df)
     result = modified_df["dilution_factor"].iloc[0]
-
-    assert result == pytest.approx(expected)
-    # assert expected == pytest.approx(result, abs=1e-1)
+    assert result == expected
 
 
-def test_calculate_dilution(data_files):
-    test_data_path = Path(data_files) / "dilution_data.csv"
+@pytest.mark.parametrize("file", test_files)
+def test_calculate_dilution(data_files, file):
+    test_data_path = Path(data_files) / file
     df = pd.read_csv(test_data_path, sep=",")
     df = calculate_dilution_factor(df)
     ion_name = "foo"
