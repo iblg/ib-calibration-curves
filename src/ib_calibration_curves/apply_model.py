b184482037a9dbc12b88f349377feddb0ceee7eb
Ian Billinge <ian.billinge@gmail.com>
2025-07-09 11:47:30 -0400

    docs: Improved documentation in apply_model.py

--- src/ib_calibration_curves/apply_model.py
+++ src/ib_calibration_curves/apply_model.py
@@ -5,16 +5,42 @@ import numpy as np
 
 
 def apply_model(
-    file_path,
+    file_path: Path,
     y,
     dy,
     model,
+    model_path: Path,
+    save_to_path: Path,
     x_column_name: str = "area",
     y_column_name: str = "concentration",
     dy_column_name: str = None,
-    model_path: Path = None,
-    overwrite_file: bool = True,
 ):
+    """
+    file_path: pathlib.Path
+    The path to the data file returned by the instrument.
+    y: function.
+        The function that will transform the instrument signal
+        to concentration.
+    dy: function.
+        The function that will transform the instrument signal
+        to uncertainty in concentration.
+    model: sm.OLS
+        The statsmodels fitting result.
+    x_column_name: str, default "area"
+        The name of the column in the file that contains the x values.
+    y_column_name: str, default "concentration"
+        The name of the column in the file where you wish to store the
+        concentration values.
+    dy_column_name: str, default None
+        The name of the column in the file where you wish to store the
+        uncertainty in concentration values.
+    model_path: Path, default None
+        The path to the model. Should not include a suffix.
+    save_to_path: bool, default True
+         The path to where you wish to save the data.
+         Options exist to overwrite the original data file are possible if
+         save_to_path is the same as file_path."
+    """
 
     if dy_column_name is None:
         dy_column_name = "d_" + y_column_name
@@ -27,16 +53,16 @@ def apply_model(
     df[fit_description] = str(model_path)
     df.loc[1:, fit_description] = np.nan
 
-    if overwrite_file:
+    if save_to_path == file_path:
         pass
-        print("Proceeding will overwrite spreadsheet.\n")
+        print("Proceeding will overwrite original data spreadsheet.\n")
         print("\nReply y to proceed.")
         response = input("Anything else will stop spreadsheet creation.\n")
         if response == "y":
             pass
         else:
             return
-    df.to_excel(file_path, index=False)
+    df.to_excel(save_to_path, index=False)
     return df
 
 
