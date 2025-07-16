5b4bbea694d856394d8fb88645d1399ee1e2d2e0
Ian Billinge <ian.billinge@gmail.com>
2025-07-16 08:17:03 -0400

    docs: added docstring to plot_results.py

--- src/ib_calibration_curves/plot_fitting_data.py
+++ src/ib_calibration_curves/plot_fitting_data.py
@@ -12,9 +12,35 @@ def plot_results(
     show_flag=False,
     save_to_path=None,
 ):
+    """
+    x_data: array-like
+    The x data to pass to the model.
+
+    y_function: function
+    The fitting function.
+
+    dy_function: function
+    The uncertainty function.
+
+    model_bounds: tuple
+    Lower and upper bounds of model parameter. If model_bounds is None, no
+    model interpolation interval is shown. If a tuple is passed, a model
+    window is shown.
+
+    show_flag: bool, default False
+    If False, no plot is shown. If True, a plot is shown.
+
+    save_to_path: str, default None
+    If None, no plot is saved. If str or Path is passed, plot is saved at
+    location specified.
+    """
     fig, ax = plt.subplots()
 
-    ax.axvspan(model_bounds[0], model_bounds[1], alpha=0.2, color="Grey")
+    if model_bounds is None:
+        pass
+    else:
+        ax.axvspan(model_bounds[0], model_bounds[1], alpha=0.2, color="Grey")
+
     ax.errorbar(x_data, y_function(x_data), yerr=dy_function(x_data), fmt="o")
     if save_to_path is None:
         pass
