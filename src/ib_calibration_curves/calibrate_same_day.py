acde8b792ad7dffdd5ead6971d448343095436f2
Ian Billinge <ian.billinge@gmail.com>
2026-08-24 15:18:23 -0400

    Changed the order of some arguments. Improved documentation as well somewhat. I don't think I made any changes to the body of the code.

--- src/ib_calibration_curves/calibrate_same_day.py
+++ src/ib_calibration_curves/calibrate_same_day.py
@@ -16,7 +16,7 @@ def check_calibrant_cols(
     data: pd.DataFrame,
     # x_col: str,
     # y_col: str,
-    std_col: str,
+    c_std_col: str,
     index_col: str,
     analyte_col: str,
     diluent_cols: str,
@@ -30,8 +30,8 @@ def check_calibrant_cols(
     if index_col not in data.columns:
         print(f"index column {index_col} not found in data")
         return False
-    if std_col not in data.columns:
-        print(f"stanard column {std_col} not found in data")
+    if c_std_col not in data.columns:
+        print(f"c_stanard column {c_std_col} not found in data")
         return False
     if analyte_col not in data.columns:
         print(f"analyte column {analyte_col} not found in data")
@@ -70,10 +70,10 @@ def save_data(data, p, idx_flag: bool = False):
 def calibrate_same_day(
     calibrant_info_path: Path,
     data_path: Path,
-    std_col: str,
-    analyte_col: str,
     analyte: str,
-    diluent_cols: list[str],
+    c_std_col: str,
+    amt_std_col: str,
+    amt_diluent_cols: list[str],
     index_col: str,
     x_col: str,
     y_col: str,
@@ -84,27 +84,63 @@ def calibrate_same_day(
     save_calibrant_data: bool = True,
     plot_results_flag: bool = True,
 ):
+    """
+
+    :param calibrant_info_path: Path or Path-like
+    Path to the sheet showing calibrant info.
+    :param data_path:
+
+    :param analyte: str.
+    Name of the analyte, i.e. 'urea' or 'h2o2'. Calibrant info sheet should
+    include a column c_std_{analyte} telling the concentration of the analyte
+    in the standard solution that was diluted to make the series.
+
+    :param std_col:
+    :param amt_analyte_col: str.
+    Name of the column in calibrant sheet showing how much the standard was
+    diluted. Examples might include "m_std" or "V_std".
+
+    :param diluent_cols: list[str]
+    List of names of columns in calibrant sheet showing how much other stuff
+    was added to the calibration series. Examples might be
+    ["V_diluent_A", "V_diluent_B"] or ["m_water"].
+    :param index_col:
+    :param x_col:
+    :param y_col:
+    :param model_save_path:
+    :param method:
+    :param y_unit:
+    :param verbose:
+    :param save_calibrant_data:
+    :param plot_results_flag:
+    :return:
+    """
     cal_data = load_data(calibrant_info_path)
 
     if check_calibrant_cols(
-        cal_data, std_col, index_col, analyte_col, diluent_cols
+        cal_data, c_std_col, index_col, amt_std_col, amt_diluent_cols
     ):
         pass
     else:
         return None
 
     if verbose:
+        print("We have opened the calibrant info file. It looks like this:")
         print(cal_data)
 
     cal_data = calculate_dilution_factor(
-        cal_data, analyte_column=analyte_col, diluent_columns=diluent_cols
+        cal_data, analyte_column=amt_std_col, diluent_columns=amt_diluent_cols
     )
     cal_data = calculate_dilution(
         cal_data, analyte=analyte, column_base_string="c_std_"
     )
 
     if verbose:
-        print(cal_data)
+        print("Dilutions have been calculated. Cal data is now:")
+        with pd.option_context(
+            "display.max_rows", None, "display.max_columns", None
+        ):
+            print(cal_data)
 
     def merge_areas_into_calibrant(cal_data):
         flattened = load_data(data_path)
